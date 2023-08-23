bb884292d3fb514a285658928aecfbfdeecf3418
tomduf <tomduf70@gmail.com>
2023-08-23 16:49:44 +0200

    V1 rouge et visible

--- src/Rampe.jsx
+++ src/Rampe.jsx
@@ -8,7 +8,7 @@ import {useControls} from "leva";
 
 export default function Rampe() {
     const {v1, v2, texte} = useControls('Versions de la rampe',{
-        v1:false,
+        v1:true,
         v2:false,
         texte:true
     })
@@ -18,10 +18,10 @@ export default function Rampe() {
     return <>
         <Clone object={escaliers.scene} scale={6} rotation={[0,0, 0]}  />
         <Clone object={rampe1.scene} scale={6} rotation={[-Math.PI/2, 0, 0]} position={[6.5, 11, 14]} visible={v1}  />
-        <Text visible={v1 && texte} color={"blue"} position={[ -0.4 , 1.2, -2]}  >
+        <Text visible={v1 && texte} color={"red"} position={[ -0.4 , 1.2, -2]}  >
             V1
         </Text>
-        <Text visible={v2 && texte} color={"brown"} position={[ -0.40 , 0, -2]} >
+        <Text visible={v2 && texte} color={"blue"} position={[ -0.40 , 0, -2]} >
             V2
         </Text>
     </>
